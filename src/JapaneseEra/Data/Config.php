beec236738e8d30165e4246b7916750a89249fcf
hazuki3417 <hazuki3417@gmail.com>
2021-02-11 01:03:08 +0900

    和暦マスタファイルを追加

---
+++ src/JapaneseEra/Data/Config.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Selen\JapaneseEra;
+
+// NOTE: 変換時の和暦文字列を変更したい場合は下記のキー値を修正してください
+return [
+    [
+        'name'      => '明治',
+        'alpha'     => 'M',
+        'startDate' => 18681023,
+        'endDate'   => 19120729,
+    ],
+    [
+        'name'      => '大正',
+        'alpha'     => 'T',
+        'startDate' => 19120730,
+        'endDate'   => 19261224,
+    ],
+    [
+        'name'      => '昭和',
+        'alpha'     => 'S',
+        'startDate' => 19261225,
+        'endDate'   => 19890107,
+    ],
+    [
+        'name'      => '平成',
+        'alpha'     => 'H',
+        'startDate' => 19890108,
+        'endDate'   => 20190430,
+    ],
+    [
+        'name'      => '令和',
+        'alpha'     => 'R',
+        'startDate' => 20190501,
+        'endDate'   => 99999999,
+    ],
+];
