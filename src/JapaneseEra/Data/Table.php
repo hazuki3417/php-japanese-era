ca438d1dac3c07e76a2d6e8c8cf3f1ad9c84535f
hazuki3417 <hazuki3417@gmail.com>
2021-02-11 15:36:23 +0900

    Tableクラスのphpdoc修正、テストコード実装

--- src/JapaneseEra/Data/Table.php
+++ src/JapaneseEra/Data/Table.php
@@ -1,11 +1,32 @@
 <?php
 
+/**
+ * @license MIT
+ * @author hazuki3417 <hazuki3417@gmail.com>
+ * @copyright 2021 hazuki3417 all rights reserved.
+ */
+
 namespace Selen\JapaneseEra\Data;
 
+/**
+ * 和暦情報一覧を保持するクラス
+ *
+ * @see \Selen\JapaneseEra\Tests\Data\TableTest テストクラス
+ */
 class Table
 {
+    /**
+     * 和暦情報一覧を保持するプロパティ
+     *
+     * @var array
+     */
     private $recordList = [];
 
+    /**
+     * 和暦情報一覧を保持したオブジェクトを作成します
+     *
+     * @return Table 新しいTableオブジェクトを返します
+     */
     public function __construct()
     {
         $config = include __DIR__ . '/Config.php';
@@ -21,12 +42,17 @@ class Table
     }
 
     /**
-     * 指定した日時の和暦情報を取得します。
+     * 引数に指定した日時の和暦情報を取得します
+     *
+     * @param \DateTime $date 取得する和暦情報の日時を渡します
+     *
+     * @example index.php description
      *
      * @return Record|null 見つかった場合はRecordを、見つからない場合はnullを返します
      */
     public function fetch(\DateTime $date)
     {
+        // FIXME: ここの処理を修正する
         /** @var Record $record */
         foreach ($this->recordList as $record) {
             if ($date < $record->startDateTime()) {
--- tests/Data/TableTest.php
+++ tests/Data/TableTest.php
@@ -1,15 +1,22 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Selen\JapaneseEra\Data\Table;
 
 /**
- * @group Selen\JapaneseEra
- * @group Selen\JapaneseEra\Data
- * @group Selen\JapaneseEra\Data\Table
+ * @group Selen
+ * @group Selen/JapaneseEra
+ * @group Selen/JapaneseEra/Data
+ * @group Selen/JapaneseEra/Data/Table
  *
  * @coversDefaultClass \Selen\JapaneseEra\Data\Table
  *
+ * @see \Selen\JapaneseEra\Data\Table ソースコード
+ *
  * @internal
+ *
+ * [Command]
+ * ./vendor/bin/phpunit --group=Selen/JapaneseEra/Data/Table
  */
 class TableTest extends TestCase
 {
@@ -27,5 +34,147 @@ class TableTest extends TestCase
      */
     protected function tearDown(): void
     {
+    }
+
+    public function testConstruct()
+    {
+        $instance = new Table();
+        $this->assertInstanceOf(Table::class, $instance);
+    }
+
+    /**
+     * 対応していない日時を指定したときのテスト
+     */
+    public function testFetchNull()
+    {
+        $instance = new Table();
+        $record   = $instance->fetch(new \DateTime('18681022'));
+
+        $this->assertNull($record);
+    }
+
+    /**
+     * 境界値テスト
+     *
+     * @dataProvider dataProviderPatten1TestFetch
+     *
+     * @param mixed $testData テストデータ
+     * @param mixed $expectedData 期待値
+     */
+    public function testFetch1($testData, $expectedData)
+    {
+        $instance = new Table();
+        $record   = $instance->fetch(new \DateTime($testData));
+
+        $verifiableData = [
+            'name'      => $record->name(),
+            'alpha'     => $record->alpha(),
+            'startDate' => $record->startDateTime()->format('Ymd'),
+            'endDate'   => $record->endDateTime()->format('Ymd'),
+        ];
+
+        $this->assertEquals($verifiableData, $expectedData);
+    }
+
+    /**
+     * 境界値テストパターン
+     *
+     * @return array
+     */
+    public function dataProviderPatten1TestFetch()
+    {
+        return [
+            // 境界値テスト
+            'testPatten1' => [
+                'testData'     => '18681023',
+                'expectedData' => $this->expectedDataMeiji(),
+            ],
+            'testPatten2' => [
+                'testData'     => '19120729',
+                'expectedData' => $this->expectedDataMeiji(),
+            ],
+            'testPatten3' => [
+                'testData'     => '19120730',
+                'expectedData' => $this->expectedDataTaisyou(),
+            ],
+            'testPatten4' => [
+                'testData'     => '19261224',
+                'expectedData' => $this->expectedDataTaisyou(),
+            ],
+            'testPatten5' => [
+                'testData'     => '19261225',
+                'expectedData' => $this->expectedDataSyouwa(),
+            ],
+            'testPatten6' => [
+                'testData'     => '19890107',
+                'expectedData' => $this->expectedDataSyouwa(),
+            ],
+            'testPatten7' => [
+                'testData'     => '19890108',
+                'expectedData' => $this->expectedDataHeisei(),
+            ],
+            'testPatten8' => [
+                'testData'     => '20190430',
+                'expectedData' => $this->expectedDataHeisei(),
+            ],
+            'testPatten9' => [
+                'testData'     => '20190501',
+                'expectedData' => $this->expectedDataReiwa(),
+            ],
+            'testPatten10' => [
+                'testData'     => '99991231',
+                'expectedData' => $this->expectedDataReiwa(),
+            ],
+        ];
+    }
+
+    public function expectedDataMeiji()
+    {
+        return [
+            'name'      => '明治',
+            'alpha'     => 'M',
+            'startDate' => '18681023',
+            'endDate'   => '19120729',
+        ];
+    }
+
+    public function expectedDataTaisyou()
+    {
+        return [
+            'name'      => '大正',
+            'alpha'     => 'T',
+            'startDate' => '19120730',
+            'endDate'   => '19261224',
+        ];
+    }
+
+    public function expectedDataSyouwa()
+    {
+        return [
+            'name'      => '昭和',
+            'alpha'     => 'S',
+            'startDate' => '19261225',
+            'endDate'   => '19890107',
+        ];
+    }
+
+    public function expectedDataHeisei()
+    {
+        return [
+            'name'      => '平成',
+            'alpha'     => 'H',
+            'startDate' => '19890108',
+            'endDate'   => '20190430',
+        ];
+    }
+
+    public function expectedDataReiwa()
+    {
+        return [
+            'name'      => '令和',
+            'alpha'     => 'R',
+            'startDate' => '20190501',
+            'endDate'   => '99991231',
+        ];
     }
 }
